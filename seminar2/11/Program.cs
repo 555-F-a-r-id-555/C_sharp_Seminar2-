594c138df69f0c4075455f0d869312247e1c18ba
Farid Bagirov <makflay85zet@gmail.com>
2023-02-17 03:41:23 +0400

    dz2 seminar

---
+++ seminar2/11/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 11: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+
+int num = Random.Shared.Next(100, 1000);
+Console.WriteLine(num);
+int first = num / 100;
+// Console.WriteLine(first);
+int last = num % 10;
+// Console.WriteLine(last);
+int result = first * 10 + last;
+
+Console.WriteLine($"{num} -> {result}");
+
---
+++ seminar2/15DZ/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+int day = Convert.ToInt16(Console.ReadLine());
+string dayoff = (day == 6 || day == 7) ? "да" : "нет";
+string error = (day <= 0 || day > 7) ? "ошибка ввода" : dayoff;
+
+Console.WriteLine($"{day} -> {error}");
+
+// Проверка ;
+// void res()
+// {
+//     for (int i = 0; i < 10; i++)
+//     {
+//         int day = i;
+//         string dayoff = (day == 6 || day == 7) ? "да" : "нет";
+//         string error = (day <= 0 || day > 7) ? "ошибка ввода" : dayoff;
+
+//         Console.WriteLine($"{day} -> {error}");
+//     }
+// }
+// res();
